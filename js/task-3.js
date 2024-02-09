1b426ae808fecc19b6a865c4ced64badb340a289
AlonaPak <146632039+AlonaPak@users.noreply.github.com>
2024-02-09 19:51:56 +0100

    end

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const nameOutput = document.getElementById("name-output");
+
+document.getElementById("name-input").addEventListener("input", (event) => {
+  const tempText = event.currentTarget.value.trim();
+  if (tempText === "") {
+    nameOutput.textContent = "Anonymous";
+  } else {
+    nameOutput.textContent = tempText;
+  }
+});
---
+++ js/task-6.js
@@ -0,0 +1,54 @@
+"use strict";
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const buttonCreate = document.body.querySelector(
+  "#controls > button[data-create]"
+);
+const buttonDestroy = document.body.querySelector(
+  "#controls > button[data-destroy]"
+);
+
+// events
+buttonCreate.addEventListener("click", function () {
+  const inputNumber = document.body.querySelector(
+    '#controls > input[type="number"]'
+  );
+  //
+  if (inputNumber.value >= 1 && inputNumber.value <= 100) {
+    createBoxes(inputNumber.value);
+  } else {
+    alert('Bad number! min="1" max="100" step="1"');
+  }
+  inputNumber.value = "";
+});
+
+buttonDestroy.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  destroyBoxes();
+  const boxes = document.getElementById("boxes");
+  let boxElements = [];
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    let last = 30;
+
+    if (boxes.lastElementChild !== null) {
+      const tempWidth = boxes.lastElementChild.style.width;
+      last = 10 + parseInt(tempWidth.slice(0, tempWidth.length - 2));
+    }
+
+    box.style.width = box.style.height = `${10 * i + last}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxElements.push(box);
+  }
+  boxes.append(...boxElements);
+}
+
+function destroyBoxes() {
+  document.querySelectorAll("div#boxes > div").forEach((el) => el.remove());
+}
